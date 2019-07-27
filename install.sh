3339cdd18f536fba0fbaaa73fb6ea5ce3ee7c197
Michael Chen <cwchen123@gmail.com>
2019-07-27 15:23:19 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+go get -u github.com/julienschmidt/httprouter
+go get -u github.com/sirupsen/logrus
+go get -u github.com/urfave/negroni
+go get -u github.com/meatballhat/negroni-logrus
+go get -u github.com/rs/cors
+go get -u github.com/jinzhu/gorm
+go get -u github.com/jinzhu/gorm/dialects/postgres
+go get -u github.com/lib/pq
